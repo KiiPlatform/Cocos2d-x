9969e486efb73f2eac9b68493ab08ae6facc9856
satoshi <satoshi.kumano@kii.com>
2014-01-31 20:15:36 +0900

    Remove binaries from repository.

--- proj.android/obj/local/armeabi/objs/cocos2dx_static/ccFPSImages.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/ccFPSImages.o: \
- /Users/guest/Documents/cocos2d-x/cocos2d-x-2.2.2/projects/MyApp/proj.android/../../../cocos2dx/ccFPSImages.c \
- /Users/guest/Documents/cocos2d-x/cocos2d-x-2.2.2/projects/MyApp/proj.android/../../../cocos2dx/ccFPSImages.h
-
-/Users/guest/Documents/cocos2d-x/cocos2d-x-2.2.2/projects/MyApp/proj.android/../../../cocos2dx/ccFPSImages.h:
